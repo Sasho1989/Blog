0ee4955d7f3c1b60cbb318375563846035b1ba24
Sasho1989 <caki.jova@gmail.com>
2020-06-05 15:06:34 +0200

    Displaying comments on articles with Comment view and form

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   # patch '/articles/:id', to: 'articles#update'
   # delete '/articles/:id', to: 'articles#destroy'
 
-  resources :articles
   root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
